9b5d0cf08ccaa18fc76ac7222b3a395adb240ab3
van-sprundel <ramonvansprundel@gmail.com>
2026-02-05 12:23:37 +0100

    chore: add concurrency bench tests with mixed r/w

--- pkg/store/mem_bench_test.go
+++ pkg/store/mem_bench_test.go
@@ -60,3 +60,70 @@ func BenchmarkMemStoreDelete(b *testing.B) {
 		store.Put(fmt.Sprintf("item-%d", i%1000), testItem{Name: "test", Value: i})
 	}
 }
+
+func BenchmarkMemStoreConcurrentReads(b *testing.B) {
+	store := NewMemStore[testItem]()
+	for i := range 1000 {
+		store.Put(fmt.Sprintf("item-%d", i), testItem{Name: fmt.Sprintf("test-%d", i), Value: i})
+	}
+
+	b.RunParallel(func(pb *testing.PB) {
+		i := 0
+		for pb.Next() {
+			store.Get(fmt.Sprintf("item-%d", i%1000))
+			i++
+		}
+	})
+}
+
+func BenchmarkMemStoreConcurrentWrites(b *testing.B) {
+	store := NewMemStore[testItem]()
+
+	b.RunParallel(func(pb *testing.PB) {
+		i := 0
+		for pb.Next() {
+			store.Put(fmt.Sprintf("item-%d", i%1000), testItem{Name: "test", Value: i})
+			i++
+		}
+	})
+}
+
+func BenchmarkMemStoreConcurrentMixed(b *testing.B) {
+	store := NewMemStore[testItem]()
+	for i := range 1000 {
+		store.Put(fmt.Sprintf("item-%d", i), testItem{Name: fmt.Sprintf("test-%d", i), Value: i})
+	}
+
+	b.RunParallel(func(pb *testing.PB) {
+		i := 0
+		for pb.Next() {
+			if i%10 == 0 {
+				// 10% writes
+				store.Put(fmt.Sprintf("item-%d", i%1000), testItem{Name: "test", Value: i})
+			} else {
+				// 90% reads
+				store.Get(fmt.Sprintf("item-%d", i%1000))
+			}
+			i++
+		}
+	})
+}
+
+func BenchmarkMemStoreConcurrentList(b *testing.B) {
+	sizes := []int{100, 1000}
+
+	for _, size := range sizes {
+		b.Run(fmt.Sprintf("size=%d", size), func(b *testing.B) {
+			store := NewMemStore[testItem]()
+			for i := range size {
+				store.Put(fmt.Sprintf("item-%d", i), testItem{Name: fmt.Sprintf("test-%d", i), Value: i})
+			}
+
+			b.RunParallel(func(pb *testing.PB) {
+				for pb.Next() {
+					_ = store.List()
+				}
+			})
+		})
+	}
+}
